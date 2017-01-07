deb433737d0a8bdd6d9735a74d7c5a936a4bffff
kbanico <code4uguam@gmail.com>
2017-01-07 01:07:26 +0000

    add update action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 5)
+    @places = Place.order("created_at ASC").all.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
@@ -19,6 +19,12 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   private
