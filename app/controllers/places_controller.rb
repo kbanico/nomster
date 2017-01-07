37061110f47ba0fe52066bfd0ba232518524c27d
kbanico <code4uguam@gmail.com>
2017-01-07 03:36:43 +0000

    destroy permissions to user who created the place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @places = Place.order("created_at ASC").all.paginate(:page => params[:page], :per_page => 5)
   end
@@ -27,7 +27,7 @@ class PlacesController < ApplicationController
   def update
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render text: "Not Allowed", status :forbidden
+      return render text: "Not Allowed", status: :forbidden
     end
     @place.update_attributes(place_params)
     redirect_to root_path
@@ -35,6 +35,11 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+
+    if @place.user !=current_user
+      return render text: "Not Allowed", status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
