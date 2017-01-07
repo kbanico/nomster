ba9dd3511f412a08d8c968af490149590e8fd46c
kbanico <code4uguam@gmail.com>
2017-01-07 07:54:28 +0000

    make submit button work to add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "places#index"
-  resources :places
+  resources :places do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
