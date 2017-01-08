9f6347bc6fc1f7b68c7297eb2a136161b45790e6
kbanico <code4uguam@gmail.com>
2017-01-08 02:56:53 +0000

    add user controller and show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments
     resources :photos
   end
+
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
